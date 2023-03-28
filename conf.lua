e576fbf299f823e8f994df067b8d99bd0d9830de
ThunderNL <stijntdm@gmail.com>
2023-03-28 21:48:44 +0200

    Added new mini icon

--- conf.lua
+++ conf.lua
@@ -3,7 +3,7 @@ function love.conf(t)
     t.window.width = 512
     t.window.height = 512
     t.window.resizable = true
-    t.window.icon = "assets/icon.png"
+    t.window.icon = "assets/miniicon.png"
     t.modules.physics = false
     
     t.console = false
